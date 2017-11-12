71dd108414bcad9d9254d3eabb9c9b20afce6498
benhadji <hadjiben@gmail.com>
2017-11-12 07:56:53 +0100

    Create project structure

---
+++ p3_escalade-consumer/src/main/java/org/oc_j2ee/projet3/consumer/App.java
@@ -0,0 +1,13 @@
+package org.oc_j2ee.projet3.consumer;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
