ac545e502d001b4a5c1d5b38aa1045f741016c75
benhadji <hadjiben@gmail.com>
2017-12-13 15:52:56 +0100

    essai lancement page web

--- p3_escalade-webapp/src/main/java/org/oc_j2ee/projet3/rest/resource/AbstractResource.java
+++ p3_escalade-webapp/src/main/java/org/oc_j2ee/projet3/rest/resource/AbstractResource.java
@@ -6,11 +6,9 @@ public abstract class AbstractResource {
 
     private static ManagerFactory managerFactory;
 
-
     public static ManagerFactory getManagerFactory() {
         return managerFactory;
     }
-
     public static void setManagerFactory(ManagerFactory pManagerFactory) {
         managerFactory = pManagerFactory;
     }
