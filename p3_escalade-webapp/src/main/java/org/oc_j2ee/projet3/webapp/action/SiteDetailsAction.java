c2066888883218d918fb1daf9acf6793cca83420
benhadji <hadjiben@gmail.com>
2017-12-25 11:31:18 +0100

    correction site/secteur/voie/longueur arborescence

--- p3_escalade-webapp/src/main/java/org/oc_j2ee/projet3/webapp/action/SiteDetailsAction.java
+++ p3_escalade-webapp/src/main/java/org/oc_j2ee/projet3/webapp/action/SiteDetailsAction.java
@@ -16,23 +16,9 @@ import java.util.List;
 public class SiteDetailsAction extends ActionSupport {
 
     private SiteManager siteManager;
-    private SecteurManager secteurManager;
-    private VoieManager voieManager;
-    private LongueurManager longueurManager;
-
     private Site site;
-    private Secteur secteur;
-    private Voie voie;
-    private Longueur longueur;
-
     private Integer site_id;
-    private Integer secteur_id;
-    private Integer voie_id;
-    private Integer longueur_id;
 
-    private List<Secteur> secteurs = new ArrayList<>();
-    private List<Voie> voies = new ArrayList<>();
-    private List<Longueur> longueurs = new ArrayList<>();
 
 
 // ******************************************* Manager ************************************** //
@@ -43,29 +29,6 @@ public class SiteDetailsAction extends ActionSupport {
         this.siteManager = siteManager;
     }
 
-    public SecteurManager getSecteurManager() {
-        return secteurManager;
-    }
-    public void setSecteurManager(SecteurManager secteurManager) {
-        this.secteurManager = secteurManager;
-    }
-
-    public VoieManager getVoieManager() {
-        return voieManager;
-    }
-    public void setVoieManager(VoieManager voieManager) {
-        this.voieManager = voieManager;
-    }
-
-    public LongueurManager getLongueurManager() {
-        return longueurManager;
-    }
-
-    public void setLongueurManager(LongueurManager longueurManager) {
-        this.longueurManager = longueurManager;
-    }
-
-
 
 // ******************************************* Model ************************************** //
     public Site getSite() {
@@ -73,28 +36,6 @@ public class SiteDetailsAction extends ActionSupport {
     }
     public void setSite(Site site) {
         this.site = site;
-    }
-
-    public Secteur getSecteur() {
-        return secteur;
-    }
-    public void setSecteur(Secteur secteur) {
-        this.secteur = secteur;
-    }
-
-    public Voie getVoie() {
-        return voie;
-    }
-    public void setVoie(Voie voie) {
-        this.voie = voie;
-    }
-
-    public Longueur getLongueur() {
-        return longueur;
-    }
-
-    public void setLongueur(Longueur longueur) {
-        this.longueur = longueur;
     }
 
 
@@ -107,64 +48,14 @@ public class SiteDetailsAction extends ActionSupport {
         this.site_id = site_id;
     }
 
-    public Integer getSecteur_id() {
-        return secteur_id;
-    }
-    public void setSecteur_id(Integer secteur_id) {
-        this.secteur_id = secteur_id;
-    }
-
-    public Integer getVoie_id() {
-        return voie_id;
-    }
-    public void setVoie_id(Integer voie_id) {
-        this.voie_id = voie_id;
-    }
-
-    public Integer getLongueur_id() {
-        return longueur_id;
-    }
-
-    public void setLongueur_id(Integer longueur_id) {
-        this.longueur_id = longueur_id;
-    }
 
 
 // ******************************************* List ************************************** //
-
-    public void setSecteurs(List<Secteur> secteurs) {
-        this.secteurs = secteurs;
-    }
-    public void setVoies(List<Voie> voies) {
-        this.voies = voies;
-    }
-    public void setLongueurs(List<Longueur> longueurs) {
-        this.longueurs = longueurs;
-    }
-
-    public List<Secteur> getSecteurs() {
-        return secteurs;
-    }
-
-
-    public List<Voie> getVoies() {
-        return voies;
-    }
-
-    public List<Longueur> getLongueurs() {
-        return longueurs;
-    }
-
-
-
 
 
     public String execute(){
 
         this.site = siteManager.getSite(this.site_id);
-        secteurs = getSecteurManager().getAllSecteursBySite(this.site);
-        voies = getVoieManager().getAllBySite(this.site);
-        longueurs = getLongueurManager().getAllBySite(this.site);
 
         return "success";
 
--- p3_escalade-webapp/src/main/webapp/siteDetails.jsp
+++ p3_escalade-webapp/src/main/webapp/siteDetails.jsp
@@ -20,42 +20,20 @@
     <div class="area-content">
 
         <p>
-            <s:property value = "site_id"/>. <s:property value = "site.nom"/>,
-            localisé à : <s:property value="site.localisation"/>
-        </p>
-
-
-        <p>
             <s:iterator value="site.secteurs">
-        <li><s:param name="secteur_id"><s:property value="secteur_id"/> </s:param>
-            secteur_id : <s:property value="secteur_id"/>.
-            site_id : <s:property value="site_id"/>.
-            nom : <s:property value = "nom"/><br></li>
-        </s:iterator>
+                <h2>Nom du secteur : <s:property value="nom"/></h2>
+                <s:iterator value="voies">
+                    <h3>Nom de la voie : <s:property value="nom"/></h3>
+                    <ul>
+                        <s:iterator value="longueurs">
+                            <li>Nom de la longueur : <s:property value="nom"/></li>
+                        </s:iterator>
+                    </ul>
+                    </p>
+                </s:iterator>
+
+            </s:iterator>
         </p>
-
-        <p>
-            <s:iterator value="secteur.voies">
-        <li><s:param name="voie_id"><s:property value="voie_id"/> </s:param>
-            voie_id : <s:property value="voie_id"/>.
-            secteur_id : <s:property value="secteur_id"/>.
-            nom : <s:property value = "nom"/>.
-            etat : <s:property value="etat"/><br></li>
-        </s:iterator>
-        </p>
-
-        <p>
-            <s:iterator value="voie.longueurs">
-        <li><s:param name="longueur_id"><s:property value="longueur_id"/> </s:param>
-            longueur_id : <s:property value="longueur_id"/>.
-            voie_id : <s:property value="voie_id"/>.
-            nom : <s:property value = "nom"/>.
-            cotation : <s:property value="cotation"/><br></li>
-        </s:iterator>
-        </p>
-
-
-
 
     </div>
 </div>
